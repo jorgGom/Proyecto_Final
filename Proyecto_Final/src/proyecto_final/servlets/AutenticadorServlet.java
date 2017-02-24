8430ee600082e9c1d87a2b85f7f42e2c0c722f27
jorgGom <jorgeifcm@gmail.com>
2017-02-24 20:52:19 +0100

    userer

--- Proyecto_Final/src/proyecto_final/servlets/AutenticadorServlet.java
+++ Proyecto_Final/src/proyecto_final/servlets/AutenticadorServlet.java
@@ -19,16 +19,13 @@ import proyecto_final.dao.UserDao;
 /**
  * Servlet implementation class AutenticadorServlet
  */
-@WebServlet({"/login","/home"})
+@WebServlet({ "/login", "/home" })
 public class AutenticadorServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
+
 	private UserDao dao;
-	
-	
-	
-       
-    @Override
+
+	@Override
 	public void init(ServletConfig config) throws ServletException {
 		// TODO Auto-generated method stub
 		super.init(config);
@@ -36,51 +33,52 @@ public class AutenticadorServlet extends HttpServlet {
 	}
 
 	/**
-     * @see HttpServlet#HttpServlet()
-     */
-    public AutenticadorServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public AutenticadorServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		request.getRequestDispatcher("index.jsp").forward(request, response);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		String newName = request.getParameter("nombre");
 		String newPass = request.getParameter("pass");
 		HttpSession session = request.getSession();
 		Usuario user = dao.validateUser(newName, newPass);
-		
-		if("admin".equalsIgnoreCase(newName) && "admin".equalsIgnoreCase(newPass)){
-			
+
+		if (user == null) {
+			doGet(request, response);
+		} 
+		else if (user != null) {
+			session.setAttribute("usuario", user);
+			response.sendRedirect("inicio");
+		}
+
+		else if ("admin".equalsIgnoreCase(newName) && "admin".equalsIgnoreCase(newPass)) {
+
 			session.setAttribute("userName", newName);
 			RequestDispatcher dispatcher = request.getRequestDispatcher("Admin");
 			dispatcher.forward(request, response);
-			
-		}
-		
-		//else if(user!=null){
-			//session.setAttribute("usuario", user);
-		//}
-		
-		else if("calvazo".equalsIgnoreCase(newName) && "1234".equalsIgnoreCase(newPass)){
-			session.setAttribute("userName", newName);
-			response.sendRedirect("inicio");
-			
-		}
-		else{
-			request.getRequestDispatcher("Contenido/error.jsp").forward(request,response);
-			
+
+		} else {
+			request.getRequestDispatcher("Contenido/error.jsp").forward(request, response);
+
 		}
 	}
 
